66e7204a133e7317b105450825a5fa232d37c1b8
OstappMelnykk <79ostap@ukr.net>
2025-02-20 03:37:02 +0200

    refactor(workload): rename fields in WorkloadModel to align with DB schema

--- src/Domain/Models/WorkloadModel.ts
+++ src/Domain/Models/WorkloadModel.ts
@@ -3,28 +3,29 @@ import {Types} from "mongoose";
 export class WorkloadModel {
     constructor(
         private _id: Types.ObjectId,
-        private _teacher: Types.ObjectId,
-        private _subject: Types.ObjectId,
+        private _teacherId: Types.ObjectId,
+        private _subjectId: Types.ObjectId,
         private _groupNumber: string,
     ) {}
 
     public static Create( id: Types.ObjectId,
-                          teacher: Types.ObjectId,
-                          subject: Types.ObjectId,
+                          teacherId: Types.ObjectId,
+                          subjectId: Types.ObjectId,
                           groupNumber: string
     ): { instance: WorkloadModel | null, error: string }
     {
         let error: string = ""
 
-        if (!teacher) error += "Teacher is not correct!\n";
-        if (!subject) error += "Subject is not correct!\n";
+
+        if (!teacherId) error += "Teacher is not correct!\n\n";
+        if (!subjectId) error += "Subject is not correct!\n";
         if (!groupNumber || groupNumber.length < 1 || groupNumber.length > 20)
             error += "GroupNumber is not correct!\n";
 
         if (error) return { instance: null, error: error };
 
         return {
-            instance: new WorkloadModel(id, teacher, subject, groupNumber),
+            instance: new WorkloadModel(id, teacherId, subjectId, groupNumber),
             error: error
         }
     }
@@ -33,11 +34,11 @@ export class WorkloadModel {
         return this._id
     }
 
-    public get teacher(){
-        return this._teacher
+    public get teacherId(){
+        return this._teacherId
     }
-    public get subject(){
-        return this._subject
+    public get subjectId(){
+        return this._subjectId
     }
     public get groupNumber(){
         return this._groupNumber
